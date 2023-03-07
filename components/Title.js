dc64cb7900e57ff3682ed3a4419b6c83b567f926
Trevor Williams <tmw7991@gmail.com>
2023-03-07 08:55:18 -0700

    url text size

--- components/Title.js
+++ components/Title.js
@@ -1,6 +1,12 @@
 function Title({ url }) {
+  let textSize = "text-xl";
+  if (url.length > 40) textSize = "text-md";
+  else if (url.length > 30) textSize = "text-lg";
+
   return (
-    <div className="flex flex-col justify-center mt-[2vh] text-xl text-center">
+    <div
+      className={`flex flex-col justify-center mt-[2vh] ${textSize} sm:text-xl text-center`}
+    >
       <div className="flex">
         <div className="font-medium mr-2">Domain:</div>
         <a href={url} target="_blank" className="text-blue-700">
