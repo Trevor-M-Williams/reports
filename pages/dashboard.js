282cbddf1cba454bd3741b5bd3ed04fd86123b2d
Trevor Williams <tmw7991@gmail.com>
2023-03-14 14:20:21 -0600

    updated dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,66 +1,43 @@
 import { useEffect, useState } from "react";
-import { getDatabase, onValue, ref, set } from "firebase/database";
+import { getReports } from "../firebase";
+import DashboardNav from "../components/DashboardNav";
+import ReportsTable from "../components/ReportsTable";
 import FileInput from "../components/FileInput";
-import { H1, H2, H3, P } from "../components/Typography";
+import SidePanel from "../components/SidePanel";
 
-function Dashboard() {
+const Dashboard = () => {
   const [reports, setReports] = useState([]);
-  const statusColors = ["bg-gray-200", "bg-green-500", "bg-yellow-300"];
+  const [currentReport, setCurrentReport] = useState(false);
+  const [uploadOpen, setUploadOpen] = useState(false);
 
   useEffect(() => {
-    const db = getDatabase();
-    const reportsRef = ref(db, "reports/");
-
-    onValue(
-      reportsRef,
-      (snapshot) => {
-        const reportsData = snapshot.val();
-        if (reportsData) {
-          const reportsList = Object.values(reportsData);
-          setReports(reportsList);
-        }
-      },
-      (error) => {
-        console.log("The read failed: " + error.name);
-      }
-    );
+    getReports(setReports);
   }, []);
 
   useEffect(() => {
-    console.log(reports[0]);
+    console.log(reports);
   }, [reports]);
 
-  function changeStatus(i) {
-    const db = getDatabase();
-    const reportsRef = ref(db, `reports/${reports[i].id}`);
-    const newStatus = reports[i].status === 2 ? 0 : reports[i].status + 1;
-    reportsRef.update({ status: newStatus });
-  }
-
   return (
-    <div className="mx-auto max-w-3xl p-2">
-      {reports.length === 0 && <FileInput />}
+    <div className="absolute inset-0 flex flex-col px-2 py-4 md:p-6">
+      <DashboardNav setUploadOpen={setUploadOpen} />
+      <FileInput
+        reports={reports}
+        setReports={setReports}
+        uploadOpen={uploadOpen}
+        setUploadOpen={setUploadOpen}
+      />
       {reports.length > 0 && (
-        <div>
-          <H1 text="Reports" />
-          {reports.map((report, i) => (
-            <div
-              key={i}
-              className="my-1 flex items-center justify-between rounded border p-2"
-            >
-              <div>{report.url}</div>
-              <div
-                onClick={() => changeStatus(i)}
-                className={`border- h-5 w-5 shrink-0 rounded-full border-white outline outline-1 outline-gray-500  ${
-                  statusColors[parseInt(report.status)]
-                }`}
-              ></div>
-            </div>
-          ))}
-        </div>
+        <>
+          <ReportsTable reports={reports} setCurrentReport={setCurrentReport} />
+          <SidePanel
+            currentReport={currentReport}
+            setCurrentReport={setCurrentReport}
+          />
+        </>
       )}
     </div>
   );
-}
+};
 
 export default Dashboard;
